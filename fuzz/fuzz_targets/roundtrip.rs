71e8b9684ac4fe4449a2de0c9b1456b2e065ad62
kryptan <kryptan@users.noreply.github.com>
2026-01-31 14:30:41 +0300

    Add fuzz tester for write/read roundtrip

---
+++ fuzz/fuzz_targets/roundtrip.rs
@@ -0,0 +1,41 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use pcx::{Reader, WriterRgb};
+
+fuzz_target!(|data: &[u8]| {
+    if data.len() < 2 {
+        return;
+    }
+    let (size, data) = data.split_at(2);
+    let width = size[0];
+    let height = size[1];
+
+    if width == 0 || height == 0 {
+        return;
+    }
+
+    let size = width as usize * height as usize * 3;
+    let mut written_pixels = vec![0; size];
+    let data_len = data.len().min(size);
+    written_pixels[..data_len].copy_from_slice(&data[..data_len]);
+
+    let mut pcx = Vec::new();
+    let mut writer = WriterRgb::new(&mut pcx, (width as u16, height as u16), (300, 300)).unwrap();
+    let row_len = width as usize * 3;
+    for y in 0..height {
+        writer
+            .write_row(&written_pixels[y as usize * row_len..(y as usize + 1) * row_len])
+            .unwrap();
+    }
+    drop(writer);
+
+    let mut reader = Reader::from_mem(&pcx).unwrap();
+    assert_eq!(reader.width(), width as u16);
+    assert_eq!(reader.height(), height as u16);
+
+    let mut read_pixels = vec![0; size];
+    reader.read_rgb_pixels(&mut read_pixels).unwrap();
+    if written_pixels != read_pixels {
+        panic!();
+    }
+});
